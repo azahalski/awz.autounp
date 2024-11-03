f9911fa856c10d21150da9c67bace34326c47aac
Andrew Zahalski <info@zahalski.dev>
2024-11-03 20:14:01 +0300

    gulp checkpoint

---
+++ update/1.0.3/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['AWZ_AUTOUNP_MODULE_NAME'] = 'AWZ: Заполнение данных по УНП';
+$MESS['AWZ_AUTOUNP_MODULE_DESCRIPTION'] = 'Модуль для заполнения названия компании и юр. адреса по УНП';
+$MESS["AWZ_PARTNER_NAME"] = "Andrew Zahalski";
+$MESS["AWZ_PARTNER_URI"] = "https://zahalski.dev";
+$MESS["AWZ_AUTOUNP_INSTALL_TITLE"] = "Удаление модуля AWZ: Заполнение данных по УНП";
