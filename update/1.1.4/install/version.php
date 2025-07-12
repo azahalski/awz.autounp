658e8d46a88e917cacd841543e73ca41f4d79698
Andrew Zahalski <andrew@zahalski.dev>
2025-07-12 13:29:45 +0300

    gulp checkpoint

---
+++ update/1.1.4/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.1.4",
+	"VERSION_DATE" => "2025-07-12 13:26:00"
+);
+?>
---
+++ update/1.1.4/lib/helper.php
@@ -0,0 +1,96 @@
+<?php
+namespace Awz\AutoUnp;
+
+use Bitrix\Main\Config\Option;
+use Bitrix\Main\Error;
+use Bitrix\Main\Result;
+use Bitrix\Main\Web\HttpClient;
+use Bitrix\Main\Web\Json;
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+class Helper {
+
+    const CACHE_TIME = 31600000;
+    const CACHE_ERR_TIME = 14400;
+    const MODULE_ID = 'awz.autounp';
+    const API_URL = 'http://grp.nalog.gov.by/api/grp-public/data';
+
+    public static function getInfo(int $unp): Result{
+        $reqResult = new \Bitrix\Main\Result();
+        if(mb_strlen((string)$unp)!==9){
+            $reqResult->addError(
+                new Error(
+                    Loc::getMessage('AWZ_AUTOUNP_HELPER_ERR_UNP'),
+                    100
+                )
+            );
+            return $reqResult;
+        }
+
+        $obCache = \Bitrix\Main\Data\Cache::createInstance();
+        if( $obCache->initCache(self::CACHE_TIME,$unp,"/awz/".self::MODULE_ID) ){
+            $res = $obCache->GetVars();
+        }elseif( $obCache->startDataCache()){
+            $r = new HttpClient([
+                'socketTimeout'=>5,
+                'streamTimeout'=>5
+            ]);
+            $params = [
+                'unp'=>$unp,
+                'charset'=>'UTF-8',
+                'type'=>'json'
+            ];
+            $res = $r->get(self::API_URL.'?'.http_build_query($params));
+            if(!$res){
+                $obCache->abortDataCache();
+            }
+            $obCache->endDataCache($res);
+        }
+
+        try{
+            $jsn = Json::decode($res);
+            $reqResult->setData($jsn);
+
+            if(isset($jsn['status'], $jsn['timestamp'])
+                && ($jsn['status']==400)
+                && (strtotime($jsn['timestamp'])<(time()-self::CACHE_ERR_TIME))
+            ){
+                $obCache = \Bitrix\Main\Data\Cache::createInstance();
+                $obCache->clean($unp, "/awz/".self::MODULE_ID);
+            }
+
+        }catch (\Exception $e){
+            $reqResult->setData(['content'=>$res]);
+            $reqResult->addError(new Error(
+                $e->getMessage(),
+                200
+            ));
+            $obCache = \Bitrix\Main\Data\Cache::createInstance();
+            $obCache->clean($unp, "/awz/".self::MODULE_ID);
+        }
+        return $reqResult;
+    }
+
+    public static function checkOptionsHash(string $siteId, string $hash): bool
+    {
+        return self::getOptionsHash($siteId) === $hash;
+    }
+
+    public static function getOptionsHash(string $siteId): string
+    {
+        static $optionsData;
+        if(!$optionsData){
+            $optionsData = [
+                Option::get(self::MODULE_ID, 'FIELD_UNP', '', $siteId),
+                Option::get(self::MODULE_ID, 'FIELD_MAME1', '', $siteId),
+                Option::get(self::MODULE_ID, 'FIELD_MAME2', '', $siteId),
+                Option::get(self::MODULE_ID, 'FIELD_UR_ADRESS', '', $siteId),
+                Option::get(self::MODULE_ID, 'PAGES', '', $siteId)
+            ];
+        }
+        return md5(serialize($optionsData));
+    }
+
+}
